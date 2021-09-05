92fd8c5cabf43958d5a05e49c8dc011226740516
Maruthi18 <syavemaruthi18@gmail.com>
2021-09-05 14:20:40 +0530

    Project Done

--- script.js
+++ script.js
@@ -10,3 +10,14 @@ $(document).ready(function(){
         $('.navbar').removeClass('nav-toggle');
     });
 });
+
+function showText(id, delay) {
+            var elem = document.getElementById(id);
+            setTimeout(function () { elem.style.visibility = 'visible'; }, delay * 1000)
+}
+window.onload = function () {
+            showText('1', 3);
+            showText('2', 5);
+            showText('3', 8);
+            showText('4', 10)
+        }
